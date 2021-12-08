49225034c13e6fe8107adf9353c80fa25a4d1c6f
Atharva Salokhe <64686082+atharva123a@users.noreply.github.com>
2021-12-08 11:52:00 +0530

    server.js now handles search requests

--- project1/project-news-app/server.js
+++ project1/project-news-app/server.js
@@ -12,7 +12,7 @@ const newsapi = new newsAPI(myKey);
 
 //use body-parser to parse data from requests:
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // timeout function to return if the message timed out:
@@ -21,36 +21,64 @@ const TIMEOUT = 10000; // 10seconds:
 // enable CORS 
 // so that your API is remotely testable 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 
 app.use(express.static('public'));
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
-app.get("/api/news", async(req, res, next)=>{
-    let t = setTimeout(()=>{
-      next({"message" : "timed out"});
-    }, TIMEOUT);
+// get top headlines from india:
+app.get("/api/news", async (req, res, next) => {
+  // to return timed out if it took too long to respond:
+  let t = setTimeout(() => {
+    next({ "message": "timed out" });
+  }, TIMEOUT);
 
-    await newsapi.v2.topHeadlines({
-      country : "in",
-      language : "en"
-    }, (err, data)=>{
-      if(err)return console.log(err);
+  // wait for the api to return topHeadlines from India: 
+  await newsapi.v2.topHeadlines({
+    country: "in",
+    language: "en"
+  }, (err, data) => {
+    // clearing timeout since we got our response!
+    clearTimeout(t);
+    if (err) return console.log(err);
 
-      res.json(data);
+    res.json(data);
 
-    })
+  })
 
 });
 
+// handle search requests:
+app.post('/api/news', async (req, res, next) => {
+  // to check if the request took too long to return:
+  let t = setTimeout(() => {
+    next({ "message": "timed out" });
+  }, TIMEOUT);
+
+  // everything returns anything related to the keyword which we specified in q:
+
+  await newsapi.v2.everything({
+    q: req.body.search,
+    language: 'en',
+    sortBy: "popularity"
+  }, (err, data) => {
+    // clearing timeout since we got our response!
+    clearTimeout(t);
+
+    // console.log error to check the error:
+    if (err) return console.log(err);
+    // else simply return data:
+    res.json(data);
+  })
+
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
